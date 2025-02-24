838677a0e6a9f9a8514c9accbb268650366bd1bf
Nethini Galagama <nethinigalagama@gmail.com>
2025-02-24 21:24:13 +0530

    Added footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import LandingPage from './components/LandingPage/LandingPage.jsx';
 import PopularHomes from './components/PopularHomes/PopularHomes.jsx';
 import WhatWeDO from './components/WeDo/WhatWeDo.jsx';
 import AboutUs from './components/AboutUs/AboutUs.jsx';
+import Footer from './components/Footer/Footer.jsx';
 import './App.css';
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <PopularHomes />
       <WhatWeDO />
       <AboutUs />
+      <Footer />
     </div>
   );
 };
