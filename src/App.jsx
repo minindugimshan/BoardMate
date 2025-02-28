355c32d5ade0f2cbe723ece0e2e400ac60226c59
minindugimshan2002 <minindugimshan11@gmail.com>
2025-03-01 00:13:13 +0530

    create and develop student profile page and landloard profile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 
 import { Route, Router, Routes } from 'react-router-dom'
 import './App.css'
-import Home from './components/Home/Home'
 import Navigationbar from './components/Navigationbar/Navigationbar'
-import Map from './components/Map/Map'
 import Footer from './components/Footer/Footer'
 import About from './components/About/About'
 
@@ -13,8 +11,6 @@ function App() {
     <>
       <Navigationbar />
       <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/Map' element={<Map />}></Route>
         <Route path='/about' element={<About />} ></Route>
       </Routes>
       <Footer />
