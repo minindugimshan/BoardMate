ec7d96ef31accc26be63c6d5db5f2d92243783e0
hasindi1230 <hasindi.20221093@iit.ac.lk>
2025-03-20 12:03:50 +0530

    Imported property data and validated if property is there or not

--- Front-End/src/components/Payments/Payments.jsx
+++ Front-End/src/components/Payments/Payments.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { propertyData } from '../../data/propertyData'
 
 function Payments() {
+
+    const {id} = useParams();
+    const propertyId = parseInt(id);
+    const property = propertyData.find((p) => p.id === propertyId);
+    const [isPopupOpen , setIsPopupOpen] = useState(false);
+
+    if(!property){
+        return <div className='text-container mt-5'>Property Not Found</div>
+    }
   return (
     <div>
-        
+
     </div>
   )
 }
