a0d48d2850cecd79fb800d551239a7c2cfc897f7
Suhanya <suhanyakuruneru9@gmail.com>
2025-03-26 00:33:54 +0530

    fixed the backend package issue

--- Back-End/boardMate/src/main/java/com/backend/boardMate/config/WebConfig.java
+++ Back-End/boardMate/src/main/java/com/backend/boardMate/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.boardMate.config;
+package com.backend.boardMate.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
