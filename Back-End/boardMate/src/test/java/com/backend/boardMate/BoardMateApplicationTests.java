eeac48a736e8eba062a9ff4d71e65e1eadab7ae9
minindugimshan2002 <minindugimshan11@gmail.com>
2025-06-20 19:56:00 +0530

    Create  class BoardMateApplicationTests

--- Back-End/boardMate/src/test/java/com/backend/boardMate/BoardMateApplicationTests.java
+++ Back-End/boardMate/src/test/java/com/backend/boardMate/BoardMateApplicationTests.java
@@ -1,28 +1,13 @@
-//package com.backend.boardMate;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class BoardMateApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
-
 package com.backend.boardMate;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.backend.boardMate.repository")
-class BoardMateApplication {
+@SpringBootTest
+class BoardMateApplicationTests {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BoardMateApplication.class, args);
+	@Test
+	void contextLoads() {
 	}
+
 }
