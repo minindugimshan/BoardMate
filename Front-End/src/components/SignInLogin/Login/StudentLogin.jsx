4143e0471205984801ce0b9f2508c6c53ca12209
minindugimshan2002 <minindugimshan11@gmail.com>
2025-06-21 23:56:36 +0530

    remove React from imports

--- Front-End/src/components/SignInLogin/Login/StudentLogin.jsx
+++ Front-End/src/components/SignInLogin/Login/StudentLogin.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import apiService from "../../../services/api-service";
