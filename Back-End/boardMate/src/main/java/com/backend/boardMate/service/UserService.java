d4f051c00abe499359f1fb829bd1dc70aad3ea3f
minindugimshan2002 <minindugimshan11@gmail.com>
2025-06-28 18:51:21 +0530

    add validation form

--- Back-End/boardMate/src/main/java/com/backend/boardMate/service/UserService.java
+++ Back-End/boardMate/src/main/java/com/backend/boardMate/service/UserService.java
@@ -5,6 +5,7 @@ import com.backend.boardMate.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
 import java.util.Optional;
 
 @Service
