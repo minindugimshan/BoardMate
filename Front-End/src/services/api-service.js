6a016497c729aa4c0a3222572b5a571d7740ac06
minindugimshan2002 <minindugimshan11@gmail.com>
2025-07-02 06:48:38 +0530

    change the base URL railway.app

--- Front-End/src/services/api-service.js
+++ Front-End/src/services/api-service.js
@@ -3,7 +3,7 @@ import { useLoaderStore } from '../store/use-loader-store';
 
 // Create an axios instance with default config
 const apiClient = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://boardmate-production.up.railway.app',
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
